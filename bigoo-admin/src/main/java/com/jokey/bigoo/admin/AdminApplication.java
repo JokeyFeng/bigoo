d71f1300615811c51b3f16fe2d6c11650c4e8e5a
JokeyFeng <976024396@qq.com>
2019-04-25 22:09:20 +0800

    创建项目

---
+++ bigoo-admin/src/main/java/com/jokey/bigoo/admin/AdminApplication.java
@@ -0,0 +1,19 @@
+package com.jokey.bigoo.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author JokeyFeng
+ * date:2019/4/25
+ * project:bigoo
+ * package:com.jokey.bigoo.admin
+ * comment:
+ */
+@SpringBootApplication
+public class AdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminApplication.class, args);
+    }
+}
---
+++ bigoo-shop/src/main/java/com/jokey/bigoo/shop/ShopApplication.java
@@ -0,0 +1,18 @@
+package com.jokey.bigoo.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author JokeyFeng
+ * date:2019/4/25
+ * project:bigoo
+ * package:com.jokey.bigoo.shop
+ * comment:
+ */
+@SpringBootApplication
+public class ShopApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ShopApplication.class, args);
+    }
+}
